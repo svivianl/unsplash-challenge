3cddc0487d5475673284c1c72f4fe8cf7a25d957
Vivian <vlshibukawa@gmail.com>
2020-05-08 09:19:45 -0400

    add infinite scroll

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,3 +1,4 @@
+import { cloneDeep } from "lodash";
 import { GET_PHOTOS, GET_PHOTOS_SUCCESS, GET_PHOTOS_ERROR } from "./types";
 
 const initialStateSearch = {
@@ -6,24 +7,32 @@ const initialStateSearch = {
   error: "",
   total: 0,
   totalPages: 0,
+  query: "",
+  page: 1,
 };
 
 export const upslashReducer = (state = initialStateSearch, action = {}) => {
+  const photosCloned = cloneDeep(state.photos);
+
   switch (action.type) {
     case GET_PHOTOS:
+      const { query, page } = action.payload;
+
       return {
         ...state,
         isPending: true,
-        photos: [],
+        photos: query === state.query ? photosCloned : [],
         error: "",
-        total: 0,
-        totalPages: 0,
+        total: query === state.query ? state.total : 0,
+        totalPages: query === state.query ? state.totalPages : 0,
+        page: query === state.query ? page : 1,
+        query,
       };
     case GET_PHOTOS_SUCCESS:
       const { results, total, total_pages } = action.payload;
       return {
         ...state,
-        photos: results,
+        photos: [...photosCloned, ...results],
         total,
         totalPages: total_pages,
         isPending: false,
