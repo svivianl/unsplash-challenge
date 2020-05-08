3ebe22445bf4ec455d9a039e76664b9bbd965453
Vivian <vlshibukawa@gmail.com>
2020-05-08 13:14:41 -0400

    add photo detail's api call

--- src/api/unsplash.api.js
+++ src/api/unsplash.api.js
@@ -1,19 +1,43 @@
 import axios from "axios";
 
-export const apiUrl = (query, page = 1) => {
-  return `https://api.unsplash.com/search/photos?page=${page}&query=${query}&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`;
+const accesskey = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
+
+export const apiUrl = (path) => {
+  return `https://api.unsplash.com${path}`;
 };
 
 export const getPhotos = async (query, page) => {
   return new Promise((resolve, reject) => {
     axios
-      .get(apiUrl(query, page))
+      .get(
+        apiUrl(
+          `/search/photos?page=${page}&query=${query}&client_id=${accesskey}`
+        )
+      )
       .then((response) => {
         if (
           (response.status === 200 && !response) ||
           (response && response.data.total === 0)
         ) {
           return reject("No images found");
+        }
+
+        return resolve(response.data);
+      })
+      .catch((error) => reject("Bad request"));
+  });
+};
+
+export const getPhotoDetails = async (photoId) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(apiUrl(`/photos/${photoId}/?client_id=${accesskey}`))
+      .then((response) => {
+        if (
+          (response.status === 200 && !response) ||
+          (response && response.data.total === 0)
+        ) {
+          return reject("Image detail found");
         }
 
         return resolve(response.data);
